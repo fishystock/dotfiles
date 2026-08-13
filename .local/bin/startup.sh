49af89f2f8649132eeb6576a1ffe3cd0cf01aea8
fishystock <215427190+fishystock@users.noreply.github.com>
2026-08-13 17:18:42 -0400

    bin: open cinny on startup

--- .local/bin/startup.sh
+++ .local/bin/startup.sh
@@ -6,5 +6,7 @@ xrandr --output DP-1 --mode 1920x1080 --rate 200
 /usr/bin/otd-daemon &
 /usr/bin/pipewire &
 /usr/bin/dunst &
-
 /usr/bin/mullvad connect &
+
+# cinny chat
+i3-msg 'workspace 1; exec /usr/bin/librewolf http://localhost:8080/'
